2671fd7d327d8a4fff588792d16a5e6bf1ab275a
crazysunj <twsunj@gmail.com>
2018-01-05 16:57:58 +0800

    注解

--- cardslideview/src/main/java/com/crazysunj/cardslideview/CardHandler.java
+++ cardslideview/src/main/java/com/crazysunj/cardslideview/CardHandler.java
@@ -25,6 +25,7 @@ import java.io.Serializable;
  * <p>构建view并处理UI逻辑
  * Created by sunjian on 2017/6/22.
  */
+@FunctionalInterface
 public interface CardHandler<T> extends Serializable {
 
     View onBind(Context context, T data, int position, @CardViewPager.TransformerMode int mode);
